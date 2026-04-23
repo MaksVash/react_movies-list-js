c120c8338330e3c7ecca3bb8101c8987bc8d22d5
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-04-23 23:30:10 +0200

    replace key

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import { MovieCard } from '../MovieCard/MovieCard';
 export const MovieList = ({ movies = [] }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
+      <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
 );
